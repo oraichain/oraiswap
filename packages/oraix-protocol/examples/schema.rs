ff33b35267712e9f6de3e7e0a93f8f3940191897
Pham Tu <tubackkhoa@gmail.com>
2022-03-18 11:22:02 +0700

    update schema

--- packages/oraix-protocol/examples/schema.rs
+++ packages/oraix-protocol/examples/schema.rs
@@ -3,7 +3,10 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use oraix_protocol::staking::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
+use oraix_protocol::staking::{
+    ConfigResponse, HandleMsg as StakingHandleMsg, InitMsg as StakingInitMsg,
+    QueryMsg as StakingQueryMsg,
+};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -11,8 +14,8 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
-    export_schema(&schema_for!(InitMsg), &out_dir);
-    export_schema(&schema_for!(HandleMsg), &out_dir);
-    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(StakingInitMsg), &out_dir);
+    export_schema(&schema_for!(StakingHandleMsg), &out_dir);
+    export_schema(&schema_for!(StakingQueryMsg), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
 }
