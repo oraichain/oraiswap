52cb8a7c3ed9692ffed344fc673d14662154c7d9
Pham Tu <tubackkhoa@gmail.com>
2022-11-08 18:24:39 +0700

    simpler sample

--- examples/factory.ts
+++ examples/factory.ts
@@ -5,10 +5,7 @@ Contract.init().then(async () => {
   const { pairs } = await Contract.factory.pairs({ limit: 10 });
 
   const ret = await Promise.all(
-    pairs.map((pair) => {
-      const pairClient = Contract.pair(pair.contract_addr);
-      return pairClient.pair();
-    })
+    pairs.map((pair) => Contract.pair(pair.contract_addr).pair())
   );
   console.log(JSON.stringify(ret, null, 2));
 });
