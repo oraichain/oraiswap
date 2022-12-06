587ce13f1a0d889d912d0b3d5202f61d8db79484
Pham Tu <tubackkhoa@gmail.com>
2022-12-06 11:30:36 +0700

    fix nested schema

--- examples/oracle.ts
+++ examples/oracle.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { Contract } from ".";
 
 Contract.init().then(async () => {
-  const ret = await Contract.oracle.treasury("tax_rate", {});
+  const ret = await Contract.oracle.treasury({ tax_cap: { denom: "kwt" } });
 
   console.log(ret);
 });
