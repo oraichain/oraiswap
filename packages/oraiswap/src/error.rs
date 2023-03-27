5c1d21f49cd097d47abb1b5f685e20be6d153a07
Hieu Le <levanhieuhust@gmail.com>
2023-03-27 11:16:43 +0700

    rename error

--- packages/oraiswap/src/error.rs
+++ packages/oraiswap/src/error.rs
@@ -71,9 +71,9 @@ pub enum ContractError {
     #[error("Order {order_id} has already fulfilled")]
     OrderFulfilled {order_id: u64},
 
-    #[error("Amount of {base_coin} must be greater than {min_base_amount}")]
-    TooSmallBaseCoinAmount {
-        base_coin: String,
-        min_base_amount: Uint128,
+    #[error("Amount of {quote_coin} must be greater than {min_quote_amount}")]
+    TooSmallQuoteAsset {
+        quote_coin: String,
+        min_quote_amount: Uint128,
     },
 }
