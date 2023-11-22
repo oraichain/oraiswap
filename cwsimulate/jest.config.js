620f96a2d5cf6d53d8cfdf2f811a84010982d06e
ducphamle2 <ducphamle212@gmail.com>
2023-11-22 13:22:00 -0800

    chore: remove cwsimulate redundant code

--- cwsimulate/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  transform: {
-    "^.+\\.ts?$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
-  },
-  testEnvironment: "node",
-};
