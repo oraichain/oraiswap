b20b90c13496d90adfacd14e303bf6cee4d10ee4
ducphamle2 <ducphamle212@gmail.com>
2024-07-25 13:09:14 -0700

    fix: mixed router schema

--- contracts/oraiswap_mixed_router/src/bin/schema.rs
+++ contracts/oraiswap_mixed_router/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use oraiswap::router::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use oraiswap::mixed_router::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
 
 fn main() {
     write_api! {
